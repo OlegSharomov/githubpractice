e60e099e7ecad4709f0611710438a064ff96d68b
OlegSharomov <sharomov.work@yandex.ru>
2022-07-09 17:57:48 +0300

    add new method

--- src/main/java/ru/git/hub/practice/controllers/FriendshipController.java
+++ src/main/java/ru/git/hub/practice/controllers/FriendshipController.java
@@ -12,4 +12,16 @@ public class FriendshipController {
     public String letsBeFriends(){
         return "Let's be friends";
     }
+
+    public String approval(){
+        System.out.println("Do you really wont to be friends?\n1 - Yes, \n2 - No");
+        int answer = scanner.nextInt();
+        if(answer == 1){
+            return "Super!";
+        } else if(answer == 2){
+            return "I'm laught at it";
+        } else{
+            return "wrong command";
+        }
+    }
 }
